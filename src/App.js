0e5ddd5229ad225ee194c73eb04d7a448b86952c
Davide Scarsi <davide@cavaliere.io>
2023-08-08 10:52:34 +0200

    cards 1 by 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import Navbar from './components/navbar';
 import Card from './components/card';
 
+import california from './images/california.png';
+import dragon from './images/dragon.png';
+import dynamite from './images/dynamite.png';
+import philadelphia from './images/philadelphia.png';
+import rainbow from './images/rainbow.png';
+import shrimp from './images/shrimp.png';
+
 
 class App extends Component {
   render() {
@@ -11,21 +18,40 @@ class App extends Component {
 
 
         <div className='container'>
-          <h1>Cosa vuoi mangiare oggi?</h1>
+          <h1>Cosa vuoi mangiare?</h1>
           <hr></hr>
           <div className='row'>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            <Card 
+            immagine ={california}
+            nome = "California"
+            prezzo = {1.99}
+            />
+            <Card 
+            immagine ={dragon}
+            nome = "Dragon"
+            prezzo = {2}
+            />
+            <Card 
+            immagine ={dynamite}
+            nome = "Dynamite"
+            prezzo = {1.3}
+            />
+            <Card 
+            immagine ={philadelphia}
+            nome = "Philadelphia"
+            prezzo = {2.4}
+            />
+            <Card 
+            immagine ={rainbow}
+            nome = "Rainbow"
+            prezzo = {0.8}
+            />
+            <Card 
+            immagine ={shrimp}
+            nome = "Shrimp"
+            prezzo = {1.4}
+            />
+
           </div>
 
         </div>
--- src/components/card.js
+++ src/components/card.js
@@ -4,12 +4,12 @@ import React, {Component} from 'react';
 class Card extends Component {
     render(){
         return (
-            <div className='col'>
+            <div className='col-4'>
                 <div className="card" style={{width: '18rem', textAligh: 'center'}}>
-                    <img src={immagine} className="card-img-top" alt="..." />
+                    <img src={this.props.immagine} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{nome} roll</h5>
-                        <p className="card-text">€{prezzo}</p>
+                        <h5 className="card-title">{this.props.nome} roll</h5>
+                        <p className="card-text">€{this.props.prezzo}</p>
                         <button className="btn btn-primary">Elimina</button>
                     </div>
                 </div>

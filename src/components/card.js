9031db92744d75dc47fc3ab14b6e523ab8126490
Davide Scarsi <davide@cavaliere.io>
2023-08-08 10:44:06 +0200

    righ brench test

--- src/components/card.js
+++ src/components/card.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react';
-import california from '../images/california.png'
+
 
 class Card extends Component {
     render(){
         return (
             <div className='col'>
                 <div className="card" style={{width: '18rem', textAligh: 'center'}}>
-                    <img src={california} className="card-img-top" alt="..." />
+                    <img src={immagine} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">Abstract</h5>
-                        <p className="card-text">€2</p>
+                        <h5 className="card-title">{nome} roll</h5>
+                        <p className="card-text">€{prezzo}</p>
                         <button className="btn btn-primary">Elimina</button>
                     </div>
                 </div>
